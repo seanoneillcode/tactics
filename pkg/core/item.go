345adfe779fd989d00691a09f5f314d917eadc56
Sean ONeill <seansaysmeh@gmail.com>
2022-05-08 13:07:46 +0100

    added some basic items

--- pkg/core/item.go
+++ pkg/core/item.go
@@ -96,11 +96,159 @@ func NewItem(name string) *Item {
 			CanConsume:  false,
 			CanEquip:    true,
 			StatEffects: []Stats{
+				{Defence: 5},
+				{Speed: -2},
+			},
+			EquipSlot: "armor",
+			ImagePath: "item/steel-armour.png",
+		}
+	case LeatherArmorItemName:
+		return &Item{
+			Type:        LeatherArmorItemName,
+			Name:        "Leather Armour",
+			Description: "A tough chest piece made from thick leather. It smells like burnt sweat.",
+			CanConsume:  false,
+			CanEquip:    true,
+			StatEffects: []Stats{
+				{Defence: 2},
+				{Speed: -1},
+			},
+			EquipSlot: "armor",
+			ImagePath: "item/leather-armour.png",
+		}
+	case ChainArmorItemName:
+		return &Item{
+			Type:        ChainArmorItemName,
+			Name:        "Chain Mail Armor",
+			Description: "A light and thin shirt of chain mail.",
+			CanConsume:  false,
+			CanEquip:    true,
+			StatEffects: []Stats{
 				{Defence: 3},
 				{Speed: -1},
 			},
 			EquipSlot: "armor",
-			ImagePath: "item/steel-armour.png",
+			ImagePath: "item/chain-mail-armour.png",
+		}
+	case BatteredSwordItemName:
+		return &Item{
+			Type:        BatteredSwordItemName,
+			Name:        "Battered Sword",
+			Description: "Dents, scratches and signs of repair. It still cuts if swung hard enough.",
+			CanConsume:  false,
+			CanEquip:    true,
+			StatEffects: []Stats{
+				{Defence: 1},
+				{AttackSkill: 2},
+			},
+			EquipSlot: "weapon",
+			ImagePath: "item/short-sword.png",
+		}
+	case RapierItemName:
+		return &Item{
+			Type:        RapierItemName,
+			Name:        "Rapier",
+			Description: "Long and gentle with a sharp tip. It's too light to parry with, but that's okay if you're fast enough.",
+			CanConsume:  false,
+			CanEquip:    true,
+			StatEffects: []Stats{
+				{AttackSkill: 4},
+				{Speed: 1},
+			},
+			EquipSlot: "weapon",
+			ImagePath: "item/short-sword.png",
+		}
+	case SaberItemName:
+		return &Item{
+			Type:        SaberItemName,
+			Name:        "Saber",
+			Description: "Curved and sharp with a little weight. It's meant for horse back, but found everywhere.",
+			CanConsume:  false,
+			CanEquip:    true,
+			StatEffects: []Stats{
+				{AttackSkill: 4},
+				{Defence: 1},
+			},
+			EquipSlot: "weapon",
+			ImagePath: "item/short-sword.png",
+		}
+	case KnifeItemName:
+		return &Item{
+			Type:        KnifeItemName,
+			Name:        "Knife",
+			Description: "A hands length of sharp steel. Used in the kitchen or the pub.",
+			CanConsume:  false,
+			CanEquip:    true,
+			StatEffects: []Stats{
+				{AttackSkill: 1},
+			},
+			EquipSlot: "weapon",
+			ImagePath: "item/short-sword.png",
+		}
+	case PineStaffItemName:
+		return &Item{
+			Type:        PineStaffItemName,
+			Name:        "Green Pine Staff",
+			Description: "A newly cut length of pine, fashioned into a staff. Springy and soft.",
+			CanConsume:  false,
+			CanEquip:    true,
+			StatEffects: []Stats{
+				{AttackSkill: 2},
+			},
+			EquipSlot: "weapon",
+			ImagePath: "item/short-sword.png",
+		}
+	case HuntingBowItemName:
+		return &Item{
+			Type:        HuntingBowItemName,
+			Name:        "Hunting Bow",
+			Description: "A common hunting bow, found in every village.",
+			CanConsume:  false,
+			CanEquip:    true,
+			StatEffects: []Stats{
+				{AttackSkill: 3},
+			},
+			EquipSlot: "weapon",
+			ImagePath: "item/short-sword.png",
+		}
+	case MagicSockItemName:
+		return &Item{
+			Type:        MagicSockItemName,
+			Name:        "Magic Sock",
+			Description: "An old sock with holes. After returning from the lost sock void, it is filled with eldritch knowledge.",
+			CanConsume:  false,
+			CanEquip:    true,
+			StatEffects: []Stats{
+				{MagicDef: 3},
+			},
+			EquipSlot: "accessory",
+			ImagePath: "item/ring.png",
+		}
+	case MagicRingItemName:
+		return &Item{
+			Type:        MagicRingItemName,
+			Name:        "Magic Ring",
+			Description: "A ring of unknown materials, it glows lightly. It shifts and warps when it's in your peripheral vision.",
+			CanConsume:  false,
+			CanEquip:    true,
+			StatEffects: []Stats{
+				{MagicSkill: 3},
+			},
+			EquipSlot: "accessory",
+			ImagePath: "item/ring.png",
+		}
+	case MagicHatItemName:
+		return &Item{
+			Type:        MagicHatItemName,
+			Name:        "Magic Hat",
+			Description: "A unfashionable hat, dusty and stained. It's feels like wearing an old couch on your head.",
+			CanConsume:  false,
+			CanEquip:    true,
+			StatEffects: []Stats{
+				{MaxMagic: 3},
+			},
+			EquipSlot: "accessory",
+			ImagePath: "item/ring.png",
 		}
 	}
 	log.Fatalf("unknown item: %s", name)
@@ -108,11 +256,43 @@ func NewItem(name string) *Item {
 }
 
 const (
+	// consumables
+
 	BreadItemName       = "bread"
 	MouldyBreadItemName = "mouldy-bread"
 	HerbItemName        = "herb"
 	PotionItemName      = "potion"
 	EtherItemName       = "ether"
-	PaddedArmorItemName = "padded-armor"
-	SteelArmorItemName  = "steel-armor"
+
+	// armor
+
+	PaddedArmorItemName  = "padded-armor"
+	SteelArmorItemName   = "steel-armor"
+	LeatherArmorItemName = "leather-armor"
+	ChainArmorItemName   = "chain-armor"
+
+	// weapons
+
+	WoodenPracticeSwordItemName = "wooden-sword"
+	BatteredSwordItemName       = "battered-sword"
+	RapierItemName              = "rapier-sword"
+	ShortSwordItemName          = "short-sword"
+	LongSwordItemName           = "long-sword"
+	SaberItemName               = "saber-sword"
+	PineStaffItemName           = "pine-staff"
+	OakStaffItemName            = "oak-staff"
+	WalnutStaffItemName         = "walnut-staff"
+	ShortBowItemName            = "short-bow"
+	LongBowItemName             = "long-bow"
+	HuntingBowItemName          = "hunting-bow"
+	KnifeItemName               = "knife"
+	ClubItemName                = "club"
+	PikeItemName                = "pike"
+	SpearItemName               = "speak"
+
+	// accessory
+
+	MagicRingItemName = "magic-ring"
+	MagicHatItemName  = "magic-hat"
+	MagicSockItemName = "magic-sock"
 )
